1920f29349935d3e18d19a0d62dde3a12cf7f9b6
Santi Pérez <90222276+Sant-TIG@users.noreply.github.com>
2021-10-12 15:14:08 +0200

    Update ft_printf.h

--- Mandatory/incs/ft_printf.h
+++ Mandatory/incs/ft_printf.h
@@ -6,11 +6,11 @@
 #include <stdio.h>
 #include <limits.h>
 
-int	ft_putlchar(char c);
-int	ft_putlstr(char *str);
-int	ft_putlnbr(int nbr);
-int	ft_putlunbr(unsigned int unbr);
-int	ft_putlhex(size_t adr, char c);
-size_t ft_strlen(char *str);
+int     ft_putlchar(char c);
+int     ft_putlstr(char *str);
+int     ft_putlnbr(int nbr);
+int     ft_putlunbr(unsigned int unbr);
+int     ft_putlhex(size_t adr, char c);
+size_t  ft_strlen(char *str);
 
 #endif
