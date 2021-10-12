17c8f414c493d1adb8120b4a056955143c03ab65
Santi Pérez <90222276+Sant-TIG@users.noreply.github.com>
2021-10-12 15:15:05 +0200

    Update ft_printf.c

--- Mandatory/ft_printf.c
+++ Mandatory/ft_printf.c
@@ -6,16 +6,21 @@
 
 int	ft_write_specifier(va_list ap, char c, int len)
 {
-	if (c =='%')
-		len += ft_putlchar('%');	
+
 	if (c == 'c')
 		len += ft_putlchar(va_arg(ap, int));
 	if (c == 's')
 		len += ft_putlstr(va_arg(ap, char *));
 	if (c == 'p')
 		len += ft_putlstr("0x") + ft_putlhex(va_arg(ap, size_t), c);
+	if (c == 'd' || c == 'i')
+		len += ft_putlnbr(va_arg(ap, int));
+	if (c == 'u')
+		len += ft_putlunbr(va_arg(ap, unsigned int));
 	if (c == 'x' || c == 'X')
 		len += ft_putlhex(va_arg(ap, unsigned int), c);
+	if (c =='%')
+		len += ft_putlchar('%');	
 	return(len);
 }
 
@@ -47,27 +52,53 @@ int main()
 	int	len = 0, len1 = 0;
 
 	printf("\n---NO %% CASE---\n");
-	len = printf("Aqui no pasamos ningun parametro adicional\n");
-	len1 = ft_printf("Aqui no pasamos ningun parametro adicional\n");
-	printf("Printf = %d\tFt_printf = %d\n", len, len1);
+	len = printf("Printf: Aqui no pasamos ningun parametro adicional\n");
+	len1 = ft_printf("Ft_printf: Aqui no pasamos ningun parametro adicional\n");
+	printf("Printf = %d\tFt_printf = %d\n", len, len1 - 3);
 	printf("\n---%% CASE---\n");
-	len = printf("%%\n");
-	len1 = ft_printf("%%\n");
-	printf("%d \t%d\n", len, len1);
+	len = printf("Printf: %%\n");
+	len1 = ft_printf("Ft_printf: %%\n");
+	printf("%d \t%d\n", len, len1 - 3);
 	ft_printf("\n---CHARACTER CASE---\n");
-	len = printf("Estos son varios caracteres: %c, %c, %c, %c, %c, %c, %c.\n", 'a','1','\0','Z',  '0' - 256, '0' + 256, ' ');
-	len1 = ft_printf("Estos son varios caracteres: %c, %c, %c, %c, %c, %c, %c.\n", 'a','1','\0','Z', '0' - 256, '0' + 256, ' ');
-	printf("Printf = %d\tFt_printf = %d\n", len, len1);
+	len = printf("Printf: Estos son varios caracteres: %c, %c, %c, %c, %c, %c, %c.\n", 'a','1','\0','Z',  '0' - 256, '0' + 256, ' ');
+	len1 = ft_printf("Ft_printf: Estos son varios caracteres: %c, %c, %c, %c, %c, %c, %c.\n", 'a','1','\0','Z', '0' - 256, '0' + 256, ' ');
+	printf("Printf = %d\tFt_printf = %d\n", len, len1 - 3);
 	ft_printf("\n---SRING CASE---\n");
-	len = printf("Estas son varias cadenas: %s, %s, %s, %s, %s.\n", "", "c", "4", "cadena ", NULL);
-	len1 = ft_printf("Estas son varias cadenas: %s, %s, %s, %s, %s.\n", "", "c", "4", "cadena ", NULL);
-	printf("Printf = %d\tFt_printf = %d\n", len, len1);
+	len = printf("Printf: Estas son varias cadenas: %s, %s, %s, %s, %s.\n", "", "c", "4", "cadena ", NULL);
+	len1 = ft_printf("Ft_printf: Estas son varias cadenas: %s, %s, %s, %s, %s.\n", "", "c", "4", "cadena ", NULL);
+	printf("Printf = %d\tFt_printf = %d\n", len, len1 - 3);
 	ft_printf("\n---POINTER CASE---\n");
-	len = printf("Estas son varias direcciones: %p, %p, %p, %p, %p, %p, %p, %p, %p, %p, %p, %p, %p, %p, %p.\n", 0, 0, NULL, NULL, -1, 1, 15, 16, 17, LONG_MAX, LONG_MIN, INT_MAX, INT_MIN, ULLONG_MAX, -ULONG_MAX);
-	len1 = ft_printf("Estas son varias direcciones: %p, %p, %p, %p, %p, %p, %p, %p, %p, %p, %p, %p, %p, %p, %p.\n", 0, 0, NULL, NULL, -1, 1, 15, 16, 17, LONG_MAX, LONG_MIN, INT_MAX, INT_MIN, ULLONG_MAX, -ULONG_MAX);
+	len = printf("Printf: Estas son varias direcciones: %p, %p, %p, %p, %p, %p, %p, %p, %p, %p, %p, %p, %p, %p, %p.\n", 0, 0, NULL, NULL, -1, 1, 15, 16, 17, LONG_MAX, LONG_MIN, INT_MAX, INT_MIN, ULLONG_MAX, -ULONG_MAX);
+	len1 = ft_printf("Ft_printf: Estas son varias direcciones: %p, %p, %p, %p, %p, %p, %p, %p, %p, %p, %p, %p, %p, %p, %p.\n", 0, 0, NULL, NULL, -1, 1, 15, 16, 17, LONG_MAX, LONG_MIN, INT_MAX, INT_MIN, ULLONG_MAX, -ULONG_MAX);
 	printf("Printf = %d\tFt_printf = %d\n", len, len1);
 	ft_printf("\n---LOWERHEX CASE---\n");
-	len = printf("Estos son varios numeros en hexadecimal txiki: %x, %x, %x, %x, %x, %x, %x, %x, %x, %x, %x, %x, %x, %x, %x.\n", 0, -1, 1, 15, 16, 17, 99, 100, 101, -15, -16, -17, -99, -100, -101);
-	len1 = ft_printf("Estos son varios numeros en hexadecimal txiki: %x, %x, %x, %x, %x, %x, %x, %x, %x, %x, %x, %x, %x, %x, %x.\n", 0, -1, 1, 15, 16, 17, 99, 100, 101, -15, -16, -17, -99, -100, -101);
-	printf("Printf = %d\tFt_printf = %d\n", len, len1);
+	len = printf("Printf: Estos son varios numeros en hexadecimal txiki: %x, %x, %x, %x, %x, %x, %x, %x, %x, %x, %x, %x, %x, %x, %x.\n", 0, -1, 1, 15, 16, 17, 99, 100, 101, -15, -16, -17, -99, -100, -101);
+	len1 = ft_printf("Ft_printf: Estos son varios numeros en hexadecimal txiki: %x, %x, %x, %x, %x, %x, %x, %x, %x, %x, %x, %x, %x, %x, %x.\n", 0, -1, 1, 15, 16, 17, 99, 100, 101, -15, -16, -17, -99, -100, -101);
+	len += printf("Printf: Estos son varios numeros en hexadecimal txiki: %x, %x, %x, %x, %x, %x, %x.\n", INT_MAX, INT_MIN, LONG_MAX, LONG_MIN, UINT_MAX, ULONG_MAX, 9223372036854775807LL);
+	len1 += ft_printf("Ft_printf: Estos son varios numeros en hexadecimal txiki: %x, %x, %x, %x, %x, %x, %x.\n", INT_MAX, INT_MIN, LONG_MAX, LONG_MIN, UINT_MAX, ULONG_MAX, 9223372036854775807LL);
+	printf("Printf = %d\tFt_printf = %d\n", len, len1 - 6);
+	ft_printf("\n---UPPERHEX CASE---\n");
+	len = printf("Printf: Estos son varios numeros en hexadecimal mayor: %X, %X, %X, %X, %X, %X, %X, %X, %X, %X, %X, %X, %X, %X, %X, %X.\n", 0, -1, 1, 15, 16, 17, 99, 100, 101, -15, -16, -17, -99, -100, -101);
+	len1 = ft_printf("Ft_printf: Estos son varios numeros en hexadecimal mayor: %X, %X, %X, %X, %X, %X, %X, %X, %X, %X, %X, %X, %X, %X, %X, %X.\n", 0, -1, 1, 15, 16, 17, 99, 100, 101, -15, -16, -17, -99, -100, -101);
+	len += printf("Printf: Estos son varios numeros en hexadecimal mayor: %X, %X, %X, %X, %X, %X, %X.\n", INT_MAX, INT_MIN, LONG_MAX, LONG_MIN, UINT_MAX, ULONG_MAX, 9223372036854775807LL);
+	len1 += ft_printf("Ft_printf: Estos son varios numeros en hexadecimal mayor: %X, %X, %X, %X, %X, %X, %X.\n", INT_MAX, INT_MIN, LONG_MAX, LONG_MIN, UINT_MAX, ULONG_MAX, 9223372036854775807LL);
+	printf("Printf = %d\tFt_printf = %d\n", len, len1 - 6);
+	ft_printf("\n---DECIMAL CASE---\n");
+	len = printf("Printf: Estos son varios numeros en hexadecimal mayor: %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d.\n", 0, -1, 1, 15, 16, 17, 99, 100, 101, -15, -16, -17, -99, -100, -101);
+	len1 = ft_printf("Ft_printf: Estos son varios numeros en hexadecimal mayor: %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d.\n", 0, -1, 1, 15, 16, 17, 99, 100, 101, -15, -16, -17, -99, -100, -101);
+	len += printf("Printf: Estos son varios numeros en hexadecimal mayor: %d, %d, %d, %d, %d, %d, %d.\n", INT_MAX, INT_MIN, LONG_MAX, LONG_MIN, UINT_MAX, ULONG_MAX, 9223372036854775807LL);
+	len1 += ft_printf("Ft_printf: Estos son varios numeros en hexadecimal mayor: %d, %d, %d, %d, %d, %d, %d.\n", INT_MAX, INT_MIN, LONG_MAX, LONG_MIN, UINT_MAX, ULONG_MAX, 9223372036854775807LL);
+	printf("Printf = %d\tFt_printf = %d\n", len, len1 - 6);
+	ft_printf("\n---INTEGER CASE---\n");
+	len = printf("Printf: Estos son varios numeros en hexadecimal mayor: %i, %i, %i, %i, %i, %i, %i, %i, %i, %i, %i, %i, %i, %i, %i.\n", 0, -1, 1, 15, 16, 17, 99, 100, 101, -15, -16, -17, -99, -100, -101);
+	len1 = ft_printf("Ft_printf: Estos son varios numeros en hexadecimal mayor: %i, %i, %i, %i, %i, %i, %i, %i, %i, %i, %i, %i, %i, %i, %i.\n", 0, -1, 1, 15, 16, 17, 99, 100, 101, -15, -16, -17, -99, -100, -101);
+	len += printf("Printf: Estos son varios numeros en hexadecimal mayor: %i, %i, %i, %i, %i, %i, %i.\n", INT_MAX, INT_MIN, LONG_MAX, LONG_MIN, UINT_MAX, ULONG_MAX, 9223372036854775807LL);
+	len1 += ft_printf("Ft_printf: Estos son varios numeros en hexadecimal mayor: %i, %i, %i, %i, %i, %i, %i.\n", INT_MAX, INT_MIN, LONG_MAX, LONG_MIN, UINT_MAX, ULONG_MAX, 9223372036854775807LL);
+	printf("Printf = %d\tFt_printf = %d\n", len, len1 - 6);
+	ft_printf("\n---UNSIGNED INT CASE---\n");
+	len = printf("Printf: Estos son varios numeros en hexadecimal mayor: %u, %u, %u, %u, %u, %u, %u, %u, %u, %u, %u, %u, %u, %u, %u.\n", 0, -1, 1, 15, 16, 17, 99, 100, 101, -15, -16, -17, -99, -100, -101);
+	len1 = ft_printf("Ft_printf: Estos son varios numeros en hexadecimal mayor: %u, %u, %u, %u, %u, %u, %u, %u, %u, %u, %u, %u, %u, %u, %u.\n", 0, -1, 1, 15, 16, 17, 99, 100, 101, -15, -16, -17, -99, -100, -101);
+	len += printf("Printf: Estos son varios numeros en hexadecimal mayor: %u, %u, %u, %u, %u, %u, %u.\n", INT_MAX, INT_MIN, LONG_MAX, LONG_MIN, UINT_MAX, ULONG_MAX, 9223372036854775807LL);
+	len1 += ft_printf("Ft_printf: Estos son varios numeros en hexadecimal mayor: %u, %u, %u, %u, %u, %u, %u.\n", INT_MAX, INT_MIN, LONG_MAX, LONG_MIN, UINT_MAX, ULONG_MAX, 9223372036854775807LL);
+	printf("Printf = %d\tFt_printf = %d\n", len, len1 - 6);
 }
