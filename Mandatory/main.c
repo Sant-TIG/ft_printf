59986c2e8d3ad7b61332e8d608c1fb7352ba06df
Santi Pérez <90222276+Sant-TIG@users.noreply.github.com>
2021-10-14 16:45:13 +0200

    Update and rename ft_printf.c to main.c

--- Mandatory/main.c
+++ Mandatory/main.c
@@ -1,52 +1,5 @@
 #include "incs/ft_printf.h"
 
-/*
-  
-*/
-
-int	ft_write_specifier(va_list ap, char c, int len)
-{
-
-	if (c == 'c')
-		len += ft_putlchar(va_arg(ap, int));
-	if (c == 's')
-		len += ft_putlstr(va_arg(ap, char *));
-	if (c == 'p')
-		len += ft_putlstr("0x") + ft_putlhex(va_arg(ap, size_t), c);
-	if (c == 'd' || c == 'i')
-		len += ft_putlnbr(va_arg(ap, int));
-	if (c == 'u')
-		len += ft_putlunbr(va_arg(ap, unsigned int));
-	if (c == 'x' || c == 'X')
-		len += ft_putlhex(va_arg(ap, unsigned int), c);
-	if (c =='%')
-		len += ft_putlchar('%');	
-	return(len);
-}
-
-/*
-  
-*/
-
-int	ft_printf(const char *format, ...)
-{
-	va_list	ap;
-	int	len;
-
-	len = 0;
-	va_start(ap, format);
-	while (*format)
-	{
-		if (*format == '%')
-			len = ft_write_specifier(ap, *++format, len);
-		else
-			len += ft_putlchar(*format);
-		format++;
-	}
-	va_end(ap);
-	return (len);
-}
-
 int main()
 {
 	int	len = 0, len1 = 0;
