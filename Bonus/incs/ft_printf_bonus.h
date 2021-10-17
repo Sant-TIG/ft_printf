9e97e8cca48f4b4fb533692bcd9e33dcf035e4c2
Santi Pérez <90222276+Sant-TIG@users.noreply.github.com>
2021-10-18 00:27:04 +0200

    Update ft_printf_bonus.h

--- Bonus/incs/ft_printf_bonus.h
+++ Bonus/incs/ft_printf_bonus.h
@@ -1,1 +1,21 @@
+#ifndef FT_PRINTF_BONUS_H
 
+# define FT_PRINTF_BONUS_H
+
+#include <unistd.h> 
+
+typedef struct printf_flags
+{
+	int	sharp;	//#
+	int	zero;	//0
+	int minus;	//-
+	int spc;	//space
+	int	plus;	//+
+	int width;	//width
+	int	prc;	//precision
+}	p_list;
+
+int ft_printf(const char *format, ...);
+int	ft_putlchar_bonus(int c, char *format, int len);
+
+#endif
