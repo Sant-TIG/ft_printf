12fbd467566ae00546048779d70d6fc3ea24035e
Santi Pérez <90222276+Sant-TIG@users.noreply.github.com>
2022-01-31 22:06:39 +0100

    Update ft_process_width.c

--- Bonus/width/ft_process_width.c
+++ Bonus/width/ft_process_width.c
@@ -1,6 +1,6 @@
 #include "../incs/ft_printf_bonus.h"
 
-int	ft_process_width(t_print *flags, char*str)
+int	ft_process_width(char*str, t_bonus *flags)
 {
 	int	nbr;
 	int	i;
@@ -8,13 +8,19 @@ int	ft_process_width(t_print *flags, char*str)
 	i = 0;
 	if (flags->id == 'c')
 		nbr = (flags->width) - 1;
-	else if (flags->id == 's' || flags->id == 'u')
+	else
 		nbr = (flags->width) - ft_strlen(str);
-	if (flags->zero == 1)
+	if (flags->zero == 1 && flags->plus == 0)
 	{
 		//printf("hgfjhgsdfohlgsv");
 		while (nbr-- > 0)
 			i += ft_putchar('0');
+		return (i);
+	}
+	else if (flags->plus == 1 || flags->space == 1)
+	{
+		while (--nbr > 0)
+			i += ft_putchar(' ');
 		return (i);
 	}
 	while (nbr-- > 0)
