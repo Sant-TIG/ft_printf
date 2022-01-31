071854c5bab249485c79c16f3c13e399bc4588de
Santi Pérez <90222276+Sant-TIG@users.noreply.github.com>
2022-01-31 22:12:52 +0100

    Create ft_init_flags.c

---
+++ Bonus/flags/ft_init_flags.c
@@ -0,0 +1,10 @@
+void	ft_init_flags(t_bonus *flags)
+{
+	//printf("INIT FLAGS\n");
+	flags->plus = 0;
+	flags->minus = 0;
+	flags->width = -1;
+	flags->space = 0;
+	flags->precision = -1;
+	flags->zero = 0;
+}
