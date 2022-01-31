c65b135ce866a409aa65262222a6031e88a70aec
Santi Pérez <90222276+Sant-TIG@users.noreply.github.com>
2022-01-31 23:21:23 +0100

    Update ft_process_precision.c

--- Bonus/precision/ft_process_precision.c
+++ Bonus/precision/ft_process_precision.c
@@ -1,19 +1,22 @@
 #include "../incs/ft_printf_bonus.h"
 
-char	*ft_process_precision(t_print *flags, char *arg)
+char	*ft_process_precision(char *str, t_bonus *flags)
 {
 	//printf("%d", flags->precision);
 	//printf ("\nPROCESS PRECISION\n");
 	char	*prc;
 	int 	nbr;
 
-	
+	if (ft_strstr(str, "(null)"))
+	{
+		free(str);
+		return ("");
+	}
 	nbr = flags->precision;
 	prc = (char *)malloc(sizeof(char) * nbr + 1);
 	prc[nbr] = '\0';
 	while (nbr-- > 0)
-		prc[nbr] = arg[nbr];
-	free(arg);
+		prc[nbr] = str[nbr];
+	free(str);
 	return (prc);
 }
-
