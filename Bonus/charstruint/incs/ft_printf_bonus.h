3d8cf4c40fb21d8e3ba26b8eb79cf80d3d9f9128
Santi Pérez <90222276+Sant-TIG@users.noreply.github.com>
2022-02-01 01:34:54 +0100

    Create ft_printf_bonus.h

---
+++ Bonus/charstruint/incs/ft_printf_bonus.h
@@ -0,0 +1,17 @@
+#ifndef FT_PRINTF_BONUS_H
+# define FT_PRINTF_BONUS_H
+
+typedef struct s_bonus
+{
+	char	id;
+	int		len;
+	int		plus;
+	int		minus;
+	int		width;
+	int		space;
+	int		precision;
+	int		zero;
+	
+} t_bonus;
+
+#endif
