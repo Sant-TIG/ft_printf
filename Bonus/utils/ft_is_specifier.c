5793e422a9ae6c30fe01a117e3572366cae13a05
Santi Pérez <90222276+Sant-TIG@users.noreply.github.com>
2022-02-07 20:16:00 +0100

    Delete Bonus/utils directory

--- Bonus/utils/ft_is_specifier.c
+++
@@ -1,5 +0,0 @@
-int	ft_is_specifier(int c)
-{
-	//printf("IS SPECIFIER\n");
-	return ((c == 'c' || c == 's' || c == 'p' || c == 'd' || c == 'i' || c == 'u' || c == 'x' || c == 'X'));
-}
--- Bonus/utils/ft_strcpy.c
+++
@@ -1,9 +0,0 @@
-#include "../incs/ft_printf_bonus.h"
-
-char	*ft_strcpy(char *dst, const char *src, size_t dstsize)
-{
-	while (*src && --dstsize)
-		*dst++ = *src++;
-	*dst = '\0';
-	return (dst);
-}
